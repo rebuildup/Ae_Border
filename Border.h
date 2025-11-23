065ca9475fa3047c1dfd276fc5e9584f8a44ddb2
rebuildup <rebuild.up.up@gmail.com>
2025-11-24 01:53:12 +0900

    Fix Border: change to RELEASE version (524289)
    - Change STAGE_VERSION from PF_Stage_DEVELOP to PF_Stage_RELEASE
    - Update PiPL version from 525313 to 524289
    - Calculation: (1<<19)|(0<<16)|(0<<13)|(0<<10)|1 = 524289
    - Fixes version mismatch error

--- Border.h
+++ Border.h
@@ -38,7 +38,7 @@
 #define	MAJOR_VERSION	1
 #define	MINOR_VERSION	0
 #define	BUG_VERSION		0
-#define	STAGE_VERSION	PF_Stage_DEVELOP
+#define	STAGE_VERSION	PF_Stage_RELEASE
 #define	BUILD_VERSION	1
 
 typedef unsigned char		u_char;
