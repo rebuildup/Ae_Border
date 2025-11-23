5be8f236e56dd88a5bc47a130d09144b04c2f36e
rebuildup <rebuild.up.up@gmail.com>
2025-11-24 02:13:28 +0900

    Fix Border: use exact version 524288 (BUILD_VERSION=0)
    - Change BUILD_VERSION from 1 to 0
    - Update PiPL version to 524288 (exactly 1<<19)
    - Fixes persistent version mismatch error (80601)

--- Border.h
+++ Border.h
@@ -39,7 +39,7 @@
 #define	MINOR_VERSION	0
 #define	BUG_VERSION		0
 #define	STAGE_VERSION	PF_Stage_RELEASE
-#define	BUILD_VERSION	1
+#define	BUILD_VERSION	0
 
 typedef unsigned char		u_char;
 typedef unsigned short		u_short;
