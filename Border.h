be2ca0e33e8f1c089351449551ddb85fb86e9729
rebuildup <rebuild.up.up@gmail.com>
2025-11-24 02:43:29 +0900

    Fix Border: version mismatch resolved
    - Changed STAGE_VERSION to PF_Stage_DEVELOP
    - Changed BUILD_VERSION to 1
    - Now matches PiPL version 528385 = PF_VERSION(1,0,0,PF_Stage_DEVELOP,1)
    - Fixes error 80600: Code version and PiPL version mismatch

--- Border.h
+++ Border.h
@@ -38,8 +38,8 @@
 #define	MAJOR_VERSION	1
 #define	MINOR_VERSION	0
 #define	BUG_VERSION		0
-#define	STAGE_VERSION	PF_Stage_RELEASE
-#define	BUILD_VERSION	0
+#define	STAGE_VERSION	PF_Stage_DEVELOP
+#define	BUILD_VERSION	1
 
 typedef unsigned char		u_char;
 typedef unsigned short		u_short;
